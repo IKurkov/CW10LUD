ed10126464b0cdac97f1c0577668f9585677c1a7
IKurkov <Ivan.Kurkov@list.ru>
2024-09-18 17:26:16 +0300

    Menu for demonstration was made

--- CW10LUD.cpp
+++ CW10LUD.cpp
@@ -1,33 +1,77 @@
 /* Kurkov Ivan, 22.B06-MM, 16.09.2024 */
+#include <conio.h>
+#include <fstream>
 #include <iostream>
+#include <random>
 
+#include "fort.hpp"
+#include "lud.hpp"
 #include "matrix.hpp"
 #include "vector.hpp"
-#include "lud.hpp"
 
 int main( void )
 {
-  Matrix<double> a(3, 3), l, u, p;
-  Vector<double> x, b(3);
-  double det;
+  bool run = true;
+  int key;
 
-  a[0][0] = 5;
-  a[0][1] = 2;
-  a[0][2] = 1;
-  a[1][0] = 4;
-  a[1][1] = 0;
-  a[1][2] = 5;
-  a[2][0] = 1;
-  a[2][1] = 2;
-  a[2][2] = 10;
+  while (run)
+  {
+    std::cout << "LU- & LUP-decompositions menu:\n"
+      << "0 - exit\n"
+      << "1 - LU-deomposition\n"
+      << "2 - LUP-decomposition\n";
+    key = _getch();
+    if (key == '0')
+      run = false;
+    if (key == '1' || key == '2')
+    {
+      size_t n, n_exp;
+      double det;
+      Matrix<double> A, L, U, P;
+      Vector<double> x, b;
+      std::default_random_engine eng;
+      std::uniform_real_distribution<> dis(-10, 10);
 
-  b[1] = 2;
-  b[2] = 19;
+      std::string fname;
+      std::ifstream in;
+      fort::char_table exp_res;
 
-  det = LUPDecomp(a, l, u, p);
-  x = LUPSolve(l, u, p, b);
-  std::cout << "L = " << l << '\n' << "U = " << u << '\n' << "P = " << p << '\n'
-    << "A = " << a << '\n' << "L * U = " << p * l * u << '\n'
-    << "A * x = " << a * x;
+      std::cout << "Input name of file with matrix: ";
+      std::cin >> fname;
+
+      in.open(fname);
+      in >> n;
+      A = Matrix<double>(n, n);
+      in >> A;
+      std::cout << "Was read as: " << A << '\n';
+      if (key == '1')
+      {
+        det = LUDecomp(A, L, U);
+        std::cout << "L = " << L << '\n' << "U = " << U << '\n'
+          << "|A - L * U|_inf = " << NormInf(A - L * U) << '\n';
+      }
+      else
+      {
+        det = LUPDecomp(A, L, U, P);
+        std::cout << "L = " << L << '\n' << "U = " << U << '\n' << "P = " << P << '\n'
+          << "|P * A - L * U|_inf = " << NormInf(P * A - L * U) << '\n';
+      }
+
+      std::cout << "Input number of experients: ";
+      std::cin >> n_exp;
+      b = Vector<double>(n);
+      exp_res << fort::header << '#' << 'b' << 'x' << "|b - Ax|_inf" << fort::endr;
+      for (size_t e = 1; e <= n_exp; e++)
+      {
+        for (size_t i = 0; i < n; i++)
+          b[i] = dis(eng);
+        x = (key == '1' ? LUSolve(L, U, b) : LUPSolve(L, U, P, b));
+        exp_res << e << b << x << NormInf(b - A * x) << fort::endr;
+      }
+      std::cout << exp_res.to_string();
+    }
+    else
+      std::cout << "[Error]: Incorrect choice!\n";
+  }
   return 0;
 }
